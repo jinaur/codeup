a28c234b2d636e2d11626312ee7995589dcd63cf
jinaur <50763720+jinaur@users.noreply.github.com>
2020-12-16 18:19:16 +0900

    1715

---
+++ 2650.py
@@ -0,0 +1,18 @@
+a, b, c = map(int, input().split())
+
+Min = min(a, b, c)
+Max = max(a, b, c)
+l = [a, b, c]
+l.remove(Max)
+l.remove(Min)
+
+if Max % Min == 0 and l[0] % Min == 0 :
+    r = Min
+else :
+    for i in range(2, Max+1) :
+        if Max % i == 0 and l[0] % i == 0 :
+            r = i
+            break
+
+print(r)
+
