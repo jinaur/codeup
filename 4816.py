b81f93a4f20b6b98a2db81bbb0de6e8358128f91
jinaur <50763720+jinaur@users.noreply.github.com>
2020-12-18 15:45:34 +0900

    1715

---
+++ 4816.py
@@ -0,0 +1,25 @@
+n = int(input())
+
+a, b, c = 0, 0, 0
+count = n
+for i in range(0, n+1) :
+    if count >= 300 :
+        a += 1
+        count -= 300
+    elif count >= 60 :
+        b += 1 
+        count -= 60
+    elif count >= 10 :
+        c += 1
+        count -= 10
+        
+    if count <= 0 :
+        break
+
+    if i >= n :
+       print(-1) 
+       exit()
+
+print(a, b, c)
+
+
