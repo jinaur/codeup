85f079db44912ec43c433d9ada7a90dfc5039a1c
jinaur <50763720+jinaur@users.noreply.github.com>
2021-01-01 15:06:04 +0900

    1715

---
+++ 1380.py
@@ -0,0 +1,19 @@
+n = input()
+
+r = 0
+if n[0] == "3" or n[0] == "6" or n[0] == "9" :
+    r += 1
+    print("K",end="")
+if len(n) >= 2 :
+    if n[1] == "3" or n[1] == "6" or n[1] == "9" :
+        r += 1
+        print("K",end="")
+if len(n) >= 3 :
+    if n[2] == "3" or n[2] == "6" or n[2] == "9" :
+        r += 1
+        print("K",end="")
+
+if r == 0 :
+    print(n)
+
+
---
+++ 1705.py
@@ -0,0 +1,8 @@
+n = int(input())
+
+for i in range(1, 7) :
+    for j in range(1, 7) :
+        if i+j == n :
+            print(i, j) 
+            break
+
