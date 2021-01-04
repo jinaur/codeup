619b58a9ecad6209363ccbb1595268a463d04b92
jinaur <50763720+jinaur@users.noreply.github.com>
2021-01-04 14:26:22 +0900

    1715

---
+++ 1116.py
@@ -0,0 +1,8 @@
+a, b = list(map(int, input().split()))
+
+print(str(a)+"+"+str(b)+"="+str(a+b))
+print(str(a)+"-"+str(b)+"="+str(a-b))
+print(str(a)+"*"+str(b)+"="+str(a*b))
+print(str(a)+"/"+str(b)+"="+str(a//b))
+
+
