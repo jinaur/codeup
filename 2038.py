58f802fc142695ebb21a45760b89d846ce637193
jinaur <50763720+jinaur@users.noreply.github.com>
2021-01-18 10:49:51 +0900

    1715

---
+++ 2038.py
@@ -0,0 +1,31 @@
+a = list(map(int, input().split()))
+b = int(input())
+c = input().split()
+d, r = list(map(int, input().split()))
+h = sum(a)
+k = str(b)
+kk = int(k)
+count = 0
+
+if c[0] == "1" and len(c) == 3 :
+    c[1] = k+c[1]
+    count = int(c[2])
+    k = eval(c[1])
+elif c[0] == "1" and len(c) != 3 :
+    c[1] = k+c[1]+c[2]
+    count = int(c[3])
+for i in range(0, h) :
+    if count >= 1 :
+        r -= int(k)
+        count -= 1
+    else :
+        r -= kk
+
+    if r <= 0 :
+        print(1)
+        break
+    h -= d
+    if h <= 0 :
+        print(0)
+        break
+    
