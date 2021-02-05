478ae24ebbd351bf66cacf4e92529909eada8294
jinaur <50763720+jinaur@users.noreply.github.com>
2021-02-05 12:07:53 +0900

    1715

--- 4013.py
+++ 4013.py
@@ -1,11 +1,29 @@
 n = int(input())
 
-a = bin(n)
-b = oct(n)
-c = hex(n)
+a = n
+a1 = ''
 
-c = c.upper()
-print(2, a[2:])
-print(8, b[2:])
-print(16, c[2:])
+while a > 0 :
+    a1 += str(a%2)
+    a = a//2
+print(2, a1[::-1])
+
+b = n
+b1 = ''
+
+while b > 0 :
+    b1 += str(b%8)
+    b = b//8
+print(8, b1[::-1])
+
+c = n
+c1 = ''
+while c > 0 : 
+    if c%16 >= 10 :
+        c1 += chr(55+c%16)
+    else :
+        c1 += str(c%16)
+    c = c//16
+
+print(16, c1[::-1])
 
