b5d72df2dd9d3eb36e98fa84a0f06c163d2ccd1f
jinaur <50763720+jinaur@users.noreply.github.com>
2021-02-21 13:48:18 +0900

    1715

--- 4751.py
+++ 4751.py
@@ -1,5 +1,24 @@
 n = int(input())
 
+Max_1 = 0
+Max_2 = 0
+Max_3 = 0
+l1 = []
+l2 = []
+l3 = []
 for i in range(0, n) :
     a, b, c = list(map(int, input().split()))
-    
+    if c > Max_1 :
+        Max_1 = c
+        l1 = [a, b]
+    elif c < Max_1 and c > Max_2 :
+        Max_2 = c
+        l2 = [a, b]
+    elif c < Max_1 and c < Max_2 and c > Max_3 and l1[0] != a and l2[0] != a :
+        Max_3 = c
+        l3 = [a, b]
+
+print(l1[0], l1[1])
+print(l2[0], l2[1])
+print(l3[0], l3[1])
+
