92dd1208e700ff9468c98cb7819c2e7d7bae8f49
jinaur <50763720+jinaur@users.noreply.github.com>
2021-02-26 14:56:16 +0900

    1715

--- 4506.py
+++ 4506.py
@@ -1,12 +1,13 @@
 a, b = list(map(int, input().split()))
 
-for i in range(10000, 2, -1) :
-    if a % i == 0 and b % i == 0 :
+for i in range(10001, 1, -1) :
+    if a%i == 0 and b%i == 0 :
         print(i)
         break
 
-for i in range(b, (a*b)+1) :
-    if i % a == 0 and i % b == 0 :
+for i in range(1, 10001) :
+    if i%a == 0 and i%b == 0 :
         print(i)
         break
+
 
