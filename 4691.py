92bb95b0976d1fcafd0a0f3792c139610d3bf9cb
jinaur <50763720+jinaur@users.noreply.github.com>
2021-02-26 15:07:35 +0900

    1715

--- 4691.py
+++ 4691.py
@@ -1,10 +1,16 @@
 n = int(input())
 
 Max = 0
+r = 0
 for i in range(0, n) :
     a, b, c, d = list(map(int, input().split()))
     if r >= Max :
         Max = r
     if a == b and b == c and c == d :
         r = 50000+a*10000
-    elif a == b and b == c or 
+    elif a == b and a == c :
+        r = 10000+a*1000
+    elif a == c and a == d :
+        r = 10000+a*1000
+    elif b == c and b == d :
+        r = 10000+a*1000
