8eaedcfc36e3f7a4e93dbc7e9cf25266474ff667
jinaur <50763720+jinaur@users.noreply.github.com>
2021-03-19 22:47:49 +0900

    1715

--- 4566.py
+++ 4566.py
@@ -3,15 +3,13 @@ m = int(input())
 
 l = []
 for i in range(n, m+1) :
-    r = True
-    if i < 2 :
-        r = False
-    for j in range(2, i) :
-        if i%j == 0 :
-            r = False
-    if r == True :
-        l.append(i)
-    
+    for j in range(1, i+1) :
+        if j != 1 and j != i and i%j == 0 :
+            break
+        else :
+            if j != 1 and j == i :
+                l.append(i)
+
 print(sum(l))
 print(min(l))
 
