42c6be68f09b459013c2b4fb4d3e5a895143ea7b
jinaur <50763720+jinaur@users.noreply.github.com>
2021-03-22 21:30:48 +0900

    1715

--- 4506.py
+++ 4506.py
@@ -1,13 +1,14 @@
 a, b = list(map(int, input().split()))
 
+def my_func(a, b) :
+    while b :
+        a,b = b,a%b
+    return a
+
 for i in range(a+1, 0, -1) :
     if a%i == 0 and b%i == 0 :
         print(i)
         break
 
-for i in range(b, a*b+1) :
-    if i%a == 0 and i%b == 0 :
-        print(i)
-        break
-
-
+r = my_func(a, b)
+print(a*b // r)
