4d99c3e534626665e9daa3f585d21487447a4c71
jinaur <50763720+jinaur@users.noreply.github.com>
2021-03-24 19:59:20 +0900

    1715

--- 4036.py
+++ 4036.py
@@ -1,13 +1,8 @@
 n = int(input())
 m = int(input())
 
-count = 0
-for i in range(1, n+1) :
-    for j in range(1, n+1) :
-        if i+j == n and abs(i-j) == m or i+j == n and abs(j-i) == m :
-            print(j)
-            print(i)
-            count = 1
-            break
-    if count != 0 :
-        break
+a = (n+m)//2
+b = a-m
+print(a)
+print(b)
+
