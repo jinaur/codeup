c3d2bcc3330644cbbab016f20079ae0fb4cf9e2a
jinaur <50763720+jinaur@users.noreply.github.com>
2021-04-02 18:33:57 +0900

    1715

--- 4866.py
+++ 4866.py
@@ -1,10 +1,14 @@
-n, m = map(int, input().split())
+n, k = list(map(int, input().split()))
 
 l1 = []
+l2 = []
+l3 = []
+l4 = []
+l5 = []
+l6 = []
 
+# s = 0 여 / s = 1 남
+r = 0
 for i in range(0, n) :
-    s, y = map(int, input().split())
-    if y == 1 or y == 2 :
-        l1.append(s)
-    if len(l1) >= m :
-    
+    s, y = list(map(int, input().split()))
+    if 
