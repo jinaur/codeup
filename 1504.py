935064015e124bd082580d933fc28abcfaf2adcc
jinaur <50763720+jinaur@users.noreply.github.com>
2021-04-06 16:09:06 +0900

    1715

--- 1504.py
+++ 1504.py
@@ -5,5 +5,6 @@ for i in range(0, n) :
         if j % 2 == 0 :
             print(i+1+(j*n), end=" ")
         elif j % 2 == 1 :
-            print(i+1+(j*n)-j, end=" ")
+            print((i-1+(j*n))*(n-i)-i+j, end=" ")
     print()
+
