ee39deba67d2a9b75f87fee874d24a0a0aca4279
jinaur <50763720+jinaur@users.noreply.github.com>
2021-04-06 15:50:26 +0900

    1715

--- 1503.py
+++ 1503.py
@@ -4,6 +4,7 @@ for i in range(0, n) :
     for j in range(0, n) :
         if (i+1) % 2 == 1 :
             print(n*i+(j+1), end=" ")
-        elif (i+1) % 2 ==  :
-            print(n*i, end=" ")
+        elif (i+1) % 2 == 0 :
+            print(n*(i+1)-j, end=" ")
     print()
+
