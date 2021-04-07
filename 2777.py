ac5718657201097b082c154e32a5fa8af2668542
jinaur <50763720+jinaur@users.noreply.github.com>
2021-04-07 17:53:15 +0900

    1715

--- 2777.py
+++ 2777.py
@@ -1,12 +1,18 @@
-while True :
-    i = 0
-    r = "00"
-    r1 = "**"
-    p, a, b = list(map(int, input().split()))
-    if p == 14 :
-        break
-    else :
-        if  
+# a = list(map(int, input().split()))
+# b = list(map(int, input().split()))
+# c = list(map(int, input().split()))
 
-    i += 1
-    
+l = [[], [], [], [], [], [], [], []]
+r = "00"
+r1 = "**"
+
+for i in range(0, 9) :
+    if i%2 == 0 :
+        for j in range(0, 4) :
+            l[i].append(r)
+            l[i].append(r1)
+    if i%2 == 1 :
+        for j in range(0, 4) :
+            l[i].append(r1)
+            l[i].append(r)
+
