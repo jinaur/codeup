71b232e5c2edd939cdcf738450beb9a4a203a554
jinaur <50763720+jinaur@users.noreply.github.com>
2021-04-07 17:30:12 +0900

    1715

--- 1524.py
+++ 1524.py
@@ -1,33 +1,37 @@
-l = [[], [], [], [], [], [], [], [], []]
+l = []
 
 for i in range(0, 9) :
     a = list(map(int, input().split()))
-    for j in range(0, 9) :
-        l[i].append(a[j])
+    l.append(a)
 
 a, b = list(map(int, input().split()))
-
+a, b = a-1, b-1
 count = 0
-if l[a-1][b-1] == 1 :
-    print(-1)
-    exit()
+# X X X
+# O   X
+# O O X
+else :
+    if l[a-1][b-1] == 1 :
+        count += 1
+    if l[a-1][b] == 1 :
+        count += 1
+    if l[a-1][b+1] == 1 :
+        count += 1
 
-if l[a-2][b-1] == 1 :
-    count += 1
-if l[a][b-1] == 1 :
-    count += 1
-if l[a-1][b] == 1 :
-    count += 1
-if l[a][b-2] == 1 :
-    count += 1
-if l[a-2][b-2] == 1 :
-    count += 1
-if l[a-2][b] == 1 :
-    count += 1
-if l[a][b-2] == 1 :
-    count += 1
+    if l[a][b-1] == 1 :
+        count += 1
+    if l[a][b+1] == 1 :
+        count += 1
+
+    if l[a+1][b-1] == 1 :
+        count += 1
+    if l[a+1][b] == 1 :
+        count += 1
+    if l[a+1][b+1] == 1 :
+        count += 1
+
 if l[a][b] == 1 :
-    count += 1
-
-print(count)
+    print(-1)
+else :
+    print(count)
 
