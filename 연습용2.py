06bac9c1c951cd8f5953b582d472f0d73f08351d
jinaur <50763720+jinaur@users.noreply.github.com>
2021-04-09 17:42:50 +0900

    1715

--- 연습용2.py
+++ 연습용2.py
@@ -1,7 +1,43 @@
-r = 0
+# 1부터 100까지 더하기
+# r = 0
+# for i in range(1, 101) :
+#     r += i
+# print(r)
 
-for i in range(1, 101) :
-    r += i 
+# 짝수일 수만 1부터 100까지 더하기
+# r = 0
+# for i in range(1, 101) :
+#     if i%2 == 0 :
+#         r += i
+# print(r)
 
-print(r)
+# 홀수인 수만 1부터 100까지 더하기
+# r = 0
+# for i in range(1, 101) :
+#     if i%2 == 1 :
+#         r += i
+# print(r)
+
+# a부터 b까지 더하기
+# a, b = list(map(int, input().split()))
+# r = 0
+# for i in range(a, b+1) :
+#     r += i
+# print(r)
+
+# 짝수인 수만 a부터 b까지 더하기
+# a, b = list(map(int, input().split()))
+# r = 0
+# for i in range(a, b+1) :
+#     if i%2 == 0 :
+#         r += i
+# print(r)
+
+# 홀수인 수만 a부터 b까지 더하기
+# a, b = list(map(int, input().split()))
+# r = 0
+# for i in range(a, b+1) :
+#     if i%2 == 1 :
+#         r += i
+# print(r)
 
