9140f34aea96e171c99d3d9ccb535a4158e2691a
jinaur <50763720+jinaur@users.noreply.github.com>
2021-04-15 17:47:18 +0900

    1715

--- 연습용2.py
+++ 연습용2.py
@@ -4,20 +4,6 @@
 #     r += i
 # print(r)
 
-# 짝수일 수만 1부터 100까지 더하기
-# r = 0
-# for i in range(1, 101) :
-#     if i%2 == 0 :
-#         r += i
-# print(r)
-
-# 홀수인 수만 1부터 100까지 더하기
-# r = 0
-# for i in range(1, 101) :
-#     if i%2 == 1 :
-#         r += i
-# print(r)
-
 # a부터 b까지 더하기
 # a, b = list(map(int, input().split()))
 # r = 0
@@ -25,19 +11,12 @@
 #     r += i
 # print(r)
 
-# 짝수인 수만 a부터 b까지 더하기
-# a, b = list(map(int, input().split()))
-# r = 0
-# for i in range(a, b+1) :
-#     if i%2 == 0 :
-#         r += i
-# print(r)
-
-# 홀수인 수만 a부터 b까지 더하기
-# a, b = list(map(int, input().split()))
-# r = 0
-# for i in range(a, b+1) :
-#     if i%2 == 1 :
-#         r += i
-# print(r)
+a, b = list(map(int, input().split()))
+def my_func(a, b, r, i) :
+    if i == b+1 :
+        return r
+    return my_func(a,b, r+i, i+1)
+    
+r = my_func(a, b, 0, 0)
+print(r)
 
