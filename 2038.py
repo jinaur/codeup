a269d460e6bc809e5a52700c6180e80574349f84
jinaur <50763720+jinaur@users.noreply.github.com>
2021-04-16 17:07:16 +0900

    1715

--- 2038.py
+++ 2038.py
@@ -7,14 +7,15 @@ k = str(b)
 kk = int(k)
 count = 0
 
-if c[0] == "1" and len(c) == 3 :
+if c[0] == "1" :
     if len(c) == 3 :
         c[1] = k+c[1]
         count = int(c[2])
         k = eval(c[1])
     else :
         c[1] = k+c[1]+c[2]
-        count = int(c[3])  
+        count = int(c[3])
+ 
 for i in range(0, h) :
     if count >= 1 :
         r -= int(k)
