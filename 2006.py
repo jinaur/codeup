24eeb56b0938cae23e36cb305c164ef3c94c6724
jinaur <50763720+jinaur@users.noreply.github.com>
2021-04-18 11:39:39 +0900

    1715

--- 2006.py
+++ 2006.py
@@ -7,8 +7,7 @@ s = int(input())
 n_count = 0
 b_count = 0
 count = 0
-for i in
- range(1, s+1) :
+for i in range(1, s+1) :
     if i%2 == 1 :
         n_count += a
         count += a
@@ -17,7 +16,6 @@ for i in
         count += b
     
     if count >= s :
-        n_count = count
         break
     
 count = 0
@@ -30,9 +28,8 @@ for i in range(1, s+1) :
         count += d
 
     if count >= s :
-        b_count = count
         break
-    
+
 if a == 10 :
     print("Tied")
 elif n_count > b_count :
